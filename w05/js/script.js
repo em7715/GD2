bd98fb7cad444e2ab75a24c20ed6fb4af723fefb
em7715 <emmaburke@ocadu.ca>
2026-03-11 18:51:46 -0400

    java script class workshop

---
+++ w05/js/script.js
@@ -0,0 +1,39 @@
+$("#preamble-btn-1").click(function(){
+    $("#preamble-btn-2").css({
+        "background":"blue",
+        "font-size":"20px",
+        "padding":"10rem",
+    })
+})
+
+
+$("#preamble-btn-2").click(function(){
+    $("#preamble-btn-1").css({
+        "background":"red",
+        "rotate":"45deg",
+
+        // $("#preamble-btn-1").toggleClass("rotate")
+    })
+})
+
+
+$(".header-left").click(function(){
+    $("#menu").toggleClass("active")
+})
+
+
+// $(document).scroll(function(){
+//     let distance = $(document).scrollTop()
+//     let height= $(document).height()- $(window).height();
+//     let ratio= distance/height;
+    
+//     $(".header-right").html(ratio)
+//     $(h1).css({
+//         "transform":"rotate("+720*ratio*360+"deg)"
+
+// })
+
+$(".album-thumbs").flickity({
+    wrapAround: true,
+    pageDots: false
+})
